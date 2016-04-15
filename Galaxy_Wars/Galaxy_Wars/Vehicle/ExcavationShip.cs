54f04adec714c351949ff1582c6856bf17eb5a55
Ahmed Ansari <ahmedansari153@gmail.com>
2016-04-15 13:19:22 -0500

    Created subclasses for vehicles, and created class vehicle

---
+++ Galaxy_Wars/Galaxy_Wars/Vehicle/ExcavationShip.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Wars.Vehicle
+{
+    class ExcavationShip : Vehicle
+    {
+        public string ShovelType { get; set; }
+    }
+}
---
+++ Galaxy_Wars/Galaxy_Wars/Vehicle/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Wars
+{
+    class Vehicle
+    {
+        public int speed { get; set; }
+        public string weapons { get; set; }
+        public bool FTL { get; set; }
+        public string armor { get; set; }
+        public string species { get; set; }
+        public int weaponPower { get; set; }
+        public string VehicleClass { get; set; }
+    }
+}

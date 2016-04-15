08a9f2c07df92a1a7ed5cf85238f31bcf51fe6f1
Ahmed Ansari <ahmedansari153@gmail.com>
2016-04-15 15:18:44 -0500

    Renames all ships

--- Galaxy_Wars/Galaxy_Wars/Vehicle/VoidRay.cs
+++ Galaxy_Wars/Galaxy_Wars/Vehicle/VoidRay.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace Galaxy_Wars
 {
-    class HeavensGate : Vehicle
+    class VoidRay : Vehicle
     {
-        public bool StealthDrive { get; set; }
+        public bool WarpSpeed { get; set; }
     }
 }
